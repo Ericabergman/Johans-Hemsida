c790d3082b3164d98d7045a28dd87929d2fa17e0
Ericabergman <erikabergman94@gmail.com>
2021-07-03 14:54:48 +0200

    finnish with contact form

--- script.js
+++ script.js
@@ -24,132 +24,52 @@ const navSlide = () => {
 
 navSlide();
 
-// var c = document.getElementById("Canvas");
-// var ctx = c.getContext("2d");
+// --------------------------------------------------
 
-// var cwidth, cheight;
-// var shells = [];
-// var pass = [];
-// var colors = [
-//   "76EA5D",
-//   "ff5252",
-//   "#ff4081",
-//   // "#e040fb",
-//   // "#fff",
-//   // "#7c4dff",
-//   // "#53dff",
-//   // "#40cdff",
-//   // "#18ffff",
-//   // "#64ffda",
-//   // "#69f0ae",
-//   // "#b2fff59",
-//   // "#775DEA",
-//   // "#CAEA5D",
-//   // "#FF0070",
-//   // "#00ECFF",
-// ];
+// CONTACT PAGE
 
-// window.onresize = function () {
-//   reset();
-// };
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
 
-// reset();
-// function reset() {
-//   cwidth = window.innerWidth;
-//   cheight = window.innerHeight;
-//   c.width = cwidth;
-//   c.height = cheight;
-// }
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
 
-// function newShell() {
-//   var left = Math.random() > 0.5;
-//   var shell = {};
-//   shell.x = 1 * left;
-//   shell.y = 1;
-//   shell.xoff = (0.01 + Math.random() * 0.007) * (left ? 1 : -1);
-//   shell.yoff = 0.01 + Math.random() * 0.007;
-//   //   shell.size = Math.random() * 6 + 3;
-//   shell.size = Math.random() * 6 + 3;
+  // Success and Error functions for after the form is submitted
 
-//   shell.color = colors[Math.floor(Math.random() * colors.length)];
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
+  }
 
-//   shells.push(shell);
-// }
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }
 
-// function newPass(shell) {
-//   var pasCount = Math.ceil(Math.pow(shell.size, 2) * Math.PI);
+  // handle the form submission event
 
-//   for (i = 0; i < pasCount; i++) {
-//     var pas = {};
-//     pas.x = shell.x * cwidth;
-//     pas.y = shell.y * cheight;
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
 
-//     var a = Math.random() * 4;
-//     var s = Math.random() * 10;
+// helper function for sending an AJAX request
 
-//     pas.xoff = s * Math.sin((5 - a) * (Math.PI / 2));
-//     pas.yoff = s * Math.sin(a * (Math.PI / 2));
-
-//     pas.color = shell.color;
-//     pas.size = Math.sqrt(shell.size);
-
-//     // if (pass.length < 1000) {
-//     if (pass.length < 10) {
-//       pass.push(pas);
-//     }
-//   }
-// }
-// var lastRun = 0;
-
-// Run();
-// function Run() {
-//   var dt = 1;
-//   if (lastRun != 0) {
-//     dt = Math.min(50, performance.now() - lastRun);
-//   }
-//   lastRun = performance.now();
-
-//   ctx.fillStyle = "rgba(0,0,0,0.25)";
-//   ctx.fillRect(0, 0, cwidth, cheight);
-
-//   if (shells.length < 10 && Math.random() > 0.96) {
-//     newShell();
-//   }
-//   for (let ix in shells) {
-//     var shell = shells[ix];
-
-//     ctx.beginPath();
-//     ctx.arc(shell.x * cwidth, shell.y * cheight, shell.size, 0, 2 * Math.PI);
-//     ctx.fillStyle = shell.color;
-//     ctx.fill();
-
-//     shell.x -= shell.xoff;
-//     shell.y -= shell.yoff;
-//     shell.xoff -= shell.xoff * dt * 0.001;
-//     shell.yoff -= (shell.yoff + 0.2) * dt * 0.00005;
-
-//     if (shell.yoff < -0.005) {
-//       newPass(shell);
-//       shells.splice(ix, 1);
-//     }
-//   }
-
-//   for (let ix in pass) {
-//     var pas = pass[ix];
-//     ctx.beginPath();
-//     ctx.arc(pas.x, pas.y, pas.size, 0, 2 * Math.PI);
-//     ctx.fillStyle = pas.color;
-//     ctx.fill();
-
-//     pas.x -= pas.xoff;
-//     pas.y -= pas.yoff;
-//     pas.xoff -= pas.xoff * dt * 0.001;
-//     pas.yoff -= (pas.yoff + 5) * dt * 0.00005;
-//     pas.size -= dt * 0.002 * Math.random();
-
-//     if (pas.y > cheight || pas.y < -50 || pas.size <= 0) {
-//       pass.splice(ix, 1);
-//     }
-//   }
-//   requestAnimationFrame(Run);
-// }
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
